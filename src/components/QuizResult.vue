b2ab87da36fa116a0439a8d2d9c94c4e0fea8b01
Ashley Cao <ashleycao12@gmail.com>
2022-04-25 16:06:51 +1200

    add component for the quiz result

---
+++ src/components/QuizResult.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>This is the result:{{result}} </h2>
+  <p>{{description}}</p>
+</template>
+
+<script>
+import {ref} from 'vue'
+import resultList from '@/assets/result-list.json'
+export default {
+  props: ['resultName'],
+  setup(props){
+    const result = ref(props.resultName)
+    const description = ref()
+    resultList.forEach(currentItem => {
+      if (currentItem.name === result.value) {
+        description.value = currentItem.description
+      }
+    });    
+    return{result, description}
+  }
+}
+</script>
+
+<style>
+
+</style>
