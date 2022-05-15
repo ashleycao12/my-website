106a9fd4e25ebb87059c1b84609a9f733f32c908
Ashley Cao <ashleycao12@gmail.com>
2022-05-16 09:46:52 +1200

    write the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
-  <h3>About Page</h3>
   <div class="content">
-    place holder
+    <img src="@/assets/profile-pic.jpg" alt="">
+    <h2>I'm Ashley, an analyst at Ministry of Education, as well as a self-taught programmer.</h2>
+    <p>I have a real passion for automation and problem solving. Wherever I've gone through out my career, I always found ways to make things more efficient and eliminate repeating tasks.</p>
+    <p>I'm making this website to learn front-end web development. I'm planning on learning some back-end stuff soon.</p>
+    <p>Outside of work I spend time on drawing and playing games. </p>
   </div>
 </template>
 
@@ -11,6 +14,22 @@ export default {
 }
 </script>
 
-<style>
- 
+<style scoped>
+.content {
+  padding: 30px;
+}
+img {
+  width: 200px;
+  border-radius: 100px;
+  border-style: solid;
+  border-color: rgb(215, 215, 215);
+  border-width: 1px;
+}
+h2 {
+  font-family: "Bogart",'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 30px;
+  color: rgb(39, 39, 39);
+  text-align: left;
+  padding: 0 50px;
+} 
 </style>
